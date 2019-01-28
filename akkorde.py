e3c2c89d189bc1bf127a7b388ff95617804218fc
TR0N <theo.reichert7@gmail.com>
2019-01-29 00:58:37 +0100

    3

--- akkorde.py
+++ akkorde.py
@@ -1,7 +1,6 @@
 import re
 
-Töne = ["c", "cis", "d", "dis", "e", "f", "fis", "g", "gis", "a", "ais", "h"]
-TöneEX = ["C","Cis","D","Dis", "E", "F","Fis","G","Gis","A","Ais","H","B","c", "cis", "d", "dis", "e", "f", "fis", "g", "gis", "a", "ais", "h", "c'" , "cis'", "d'", "dis'", "e'", "f'", "fis'", "g'", "gis'", "a'", "ais'", "h'"]
+Töne = ["C","Cis","D","Dis", "E", "F","Fis","G","Gis","A","Ais","H","B","c", "cis", "d", "dis", "e", "f", "fis", "g", "gis", "a", "ais", "h", "c'" , "cis'", "d'", "dis'", "e'", "f'", "fis'", "g'", "gis'", "a'", "ais'", "h'"]
 Tonal = ["dur", "moll"]
 def Prime(x):
     return x
@@ -32,44 +31,34 @@ def großeSeptime(x):
 def reineOktave(x):
     return x + 12
 def durdreiklang(x):
-    print(Töne[x] + " " + Töne[großeTerz(x)] + " " + Töne[reineQuarte(x)])
+    return(Töne[x] + " " + Töne[großeTerz(x)] + " " + Töne[reineQuarte(x)])
 def molldreiklang(x):
-    print(Töne[x] + " " + Töne[kleineTerz(x)] + " " + Töne[reineQuinte(x)])
+    return(Töne[x] + " " + Töne[kleineTerz(x)] + " " + Töne[reineQuinte(x)])
 def Dominantseptakkord(x):
-    durdreiklang(x)
-    print(Töne[kleineSeptime(x)])
+    return durdreiklang(x) + " " + Töne[kleineSeptime(x)]
 def großerSeptakkord(x):
-    durdreiklang(x)
-    print(großeSeptime(x))
+    return durdreiklang(x) + " " + großeSeptime(x)
 def Mollseptakkord(x):
-    molldreiklang(x)
-    print(Töne[kleineSeptime(x)])
+    return molldreiklang(x) + " " + Töne[kleineSeptime(x)]
 def Mollseptakkord_mit_großerSeptime(x):
-    molldreiklang(x)
-    print(Töne[großeSeptime(x)])
+    return molldreiklang(x) + " " + Töne[großeSeptime(x)]
 
 userinput = input("Grundton Tonalität")
 liste = re.split(" ", userinput)
 eingabeGrundton = liste[0]
 eingabeTonalität = liste[1]
 
-for e in Töne:
-    if e == eingabeGrundton:
-        global Grundton
-        Grundton = Töne.index(e)
-        break
-    else:
-        pass
+if eingabeGrundton in Töne:
+    Grundton = eingabeGrundton
+else:
+    print("Tonname falsch")
 
-for e in Tonal:
-    if e == eingabeTonalität:
-        global Tonalität
-        Tonalität = Tonal.index(e)
-        break
-    else:
-        pass
+if eingabeTonalität in Tonal:
+    Tonalität = eingabeTonalität
+else:
+    print("eigegebene Tonalität falsch")
 
-if Tonalität == 0:
+if Tonalität == "dur":
     print(durdreiklang(Grundton))
-elif Tonalität == 1:
+elif Tonalität == "moll":
     print(molldreiklang(Grundton))
