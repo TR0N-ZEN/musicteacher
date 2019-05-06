5548528f3ff40ce2135d0d042835b192eb6156c4
Theo <theo.reichert7@gmail.com>
2019-05-06 22:58:51 +0200

    42

--- test.py
+++ test.py
@@ -1,26 +1,5 @@
-# tones = ["C", "Cis", "D", "Dis", "E", "F", "Fis", "G", "Gis", "A", "Ais", "H", "c", "cis", "d", "dis", "e", "f", "fis", "g", "gis", "a", "ais", "h", "c'" , "cis'", "d'", "dis'", "e'", "f'", "fis'", "g'", "gis'", "a'", "ais'", "h'"]
+tuple1 = (1, 2, 3, 4)
+print(type(tuple1))
 
-# def major(x):
-#     return x, x+2, x+4, x+5, x+7, x+9, x+11, x+12
-
-# def translator(tupleofintegers):
-#     # listoftonesrequested = []
-#     for e in tupleofintegers:
-#         print(tones[e])
-#         # listoftonesrequested.append(tones[e])
-#     # return list listoftonesrequested
-
-# root = input()
-# rootinteger = tones.index(root)
-# tonality = input()
-
-# dicto = {
-#     "major": major(rootinteger)
-# }
-
-# translator(dicto.get(tonality))
-class Person:
-  def myfunc(self):
-    print("Hello my name is ")
-
-Person().myfunc()
+for e in tuple1:
+  print(type(e))
